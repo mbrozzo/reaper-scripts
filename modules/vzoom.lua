665973d8da42ccfd019d170bb366b41045bdff28
mbrozzo <mbrozzo@gmail.com>
2025-04-30 00:58:54 +0200

    Restore track height overrides in execute_with_current_track_heights_locked

--- modules/vzoom.lua
+++ modules/vzoom.lua
@@ -12,19 +12,24 @@ function vzoom.execute_with_current_track_heights_locked(func, ...)
 	-- Lock track heights and save lock state
 	local master_track = reaper.GetMasterTrack(0)
 	local master_track_lock = reaper.GetMediaTrackInfo_Value(master_track, "B_HEIGHTLOCK")
-	reaper.SetMediaTrackInfo_Value(master_track, "I_HEIGHTOVERRIDE", reaper.GetMediaTrackInfo_Value(master_track, "I_TCPH"))
+	local master_track_override = reaper.GetMediaTrackInfo_Value(master_track, "I_HEIGHTOVERRIDE")
+	reaper.SetMediaTrackInfo_Value(master_track, "I_HEIGHTOVERRIDE",
+		reaper.GetMediaTrackInfo_Value(master_track, "I_TCPH"))
 	reaper.SetMediaTrackInfo_Value(master_track, "B_HEIGHTLOCK", 1)
 	local n_tracks = reaper.CountTracks(0)
 	if n_tracks < 0 then return end
 	local locks = {}
+	local overrides = {}
 	for i = 0, n_tracks - 1 do
 		local track = reaper.GetTrack(0, i)
 		locks[i] = reaper.GetMediaTrackInfo_Value(track, "B_HEIGHTLOCK")
-		reaper.SetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE", reaper.GetMediaTrackInfo_Value(track, "I_TCPH"))
+		overrides[i] = reaper.GetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE")
+		reaper.SetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE",
+			reaper.GetMediaTrackInfo_Value(track, "I_TCPH"))
 		reaper.SetMediaTrackInfo_Value(track, "B_HEIGHTLOCK", 1)
 	end
 
-	local retvals = {func(...)}
+	local retvals = { func(...) }
 
 	-- Restore toggle command states
 	if reaper.GetToggleCommandState(MINIMIZE_TOGGLE_COMMAND) ~= minimize_toggle_command_state then
@@ -40,13 +45,19 @@ function vzoom.execute_with_current_track_heights_locked(func, ...)
 		reaper.SetMediaTrackInfo_Value(reaper.GetTrack(0, i), "B_HEIGHTLOCK", locks[i])
 	end
 
+	-- Restore track height overrides
+	reaper.SetMediaTrackInfo_Value(reaper.GetMasterTrack(0), "I_HEIGHTOVERRIDE", master_track_override)
+	for i = 0, n_tracks - 1 do
+		reaper.SetMediaTrackInfo_Value(reaper.GetTrack(0, i), "I_HEIGHTOVERRIDE", overrides[i])
+	end
+
 	reaper.TrackList_AdjustWindows(false)
 
 	return table.unpack(retvals)
 end
 
 function vzoom.calculate_current_default_track_height_steps()
-	return vzoom.execute_with_current_track_heights_locked(function ()
+	return vzoom.execute_with_current_track_heights_locked(function()
 		-- Insert a new track at the top of the track control panel
 		reaper.InsertTrackAtIndex(0, false)
 
@@ -91,7 +102,7 @@ function vzoom.get_current_min_track_height()
 end
 
 function vzoom.get_current_max_track_height()
-	return vzoom.execute_with_current_track_heights_locked(function ()
+	return vzoom.execute_with_current_track_heights_locked(function()
 		-- Insert a new track at the top of the track control panel
 		reaper.InsertTrackAtIndex(0, false)
 
@@ -109,23 +120,17 @@ function vzoom.get_current_max_track_height()
 	end)
 end
 
--- Alias for get_max_track_h
-function vzoom.get_tcp_height()
-	return vzoom.get_current_max_track_height()
-end
-
--- Alias for get_max_track_h
-function vzoom.get_arrange_view_height()
-	return vzoom.get_current_max_track_height()
-end
+-- Aliased for get_max_track_h
+vzoom.get_tcp_height = vzoom.get_current_max_track_height
+vzoom.get_arrange_view_height = vzoom.get_current_max_track_height
 
 --[[
 	New heights are calculated as follows: `new_height = old_height + old_height * scale_factor + height_increase`.
 	The maximum track height is considered as the height of the arrange view.
 ]]
 function vzoom.zoom_vertically_preserving_track_heights(
-	scale_factor,      --number, optional (default: 1), scaling factor
-	height_increase   --umber, optional (default: 0), height increase
+	scale_factor, --number, optional (default: 1), scaling factor
+	height_increase --umber, optional (default: 0), height increase
 )
 	-- Set default values
 	scale_factor = scale_factor or 1
@@ -135,6 +140,18 @@ function vzoom.zoom_vertically_preserving_track_heights(
 	if n_tracks <= 0 then return end
 
 	local min_h = vzoom.get_current_min_track_height()
+
+	-- Master track height
+	local master_height
+	master_height = reaper.GetMediaTrackInfo_Value(reaper.GetMasterTrack(0), "I_TCPH")
+	local locked = reaper.GetMediaTrackInfo_Value(reaper.GetMasterTrack(0), "B_HEIGHTLOCK")
+	-- Ignore if locked
+	if locked == 0 then
+		-- Calculate new height
+		master_height = math.max(master_height * scale_factor + height_increase, min_h)
+	end
+
+	-- Other track heights
 	local heights = {}
 	for i = 0, n_tracks - 1 do
 		heights[i] = reaper.GetMediaTrackInfo_Value(reaper.GetTrack(0, i), "I_TCPH")
@@ -144,14 +161,12 @@ function vzoom.zoom_vertically_preserving_track_heights(
 		if locked ~= 0 then goto continue end
 
 		-- Calculate new height
-		new_h = heights[i] + heights[i] * scale_factor + height_increase
-
-		-- Set new height enforcing min height
-		heights[i] = math.max(new_h, min_h)
+		heights[i] = math.max(heights[i] * scale_factor + height_increase, min_h)
 		::continue::
 	end
 
 	reaper.PreventUIRefresh(1)
+	reaper.SetMediaTrackInfo_Value(reaper.GetMasterTrack(0), "I_HEIGHTOVERRIDE", master_height)
 	for i = 0, n_tracks - 1 do
 		reaper.SetMediaTrackInfo_Value(reaper.GetTrack(0, i), "I_HEIGHTOVERRIDE", heights[i])
 	end
